f1f5dedeb5abac3ad8d7454b67efdfb47c3034c8
Tyler Ireland <111758942+tylerireland@users.noreply.github.com>
2023-04-12 13:25:46 -0400

    changed port number

--- deployment/redeploy.sh
+++ deployment/redeploy.sh
@@ -3,4 +3,4 @@
 docker stop site
 docker rm site
 docker pull tyleriireland/project4:latest
-docker run -d -p 8080:80 --name site tyleriireland/project4
+docker run -d -p 8000:80 --name site tyleriireland/project4
